Cqt runs, but returns incorrect values. Check body of for loop in nsgtf_real: line 76.

mcft_python/cqt_toolbox/cqt.py
@@ -7,7 +7,7 @@ from nsgtf_real import  nsgtf_real
 
 
 def cqt(x, B, fs, fmin, fmax,
-			rasterize='full', phasemode='global', format='sparse',
+			rasterize='full', phasemode='global', outputFormat='sparse',
 			gamma=0, normalize='sine', win_fun='hann'):
 	'''
 	%CQT  Constant-Q/Variable-Q transform
@@ -94,14 +94,13 @@ def cqt(x, B, fs, fmin, fmax,
 	'''
 
 	g,shift,M = nsgcqwin(fmin,fmax,B,fs,len(x), winfun=win_fun, gamma=gamma)
-	import pdb; pdb.set_trace()
 
 	total_bins = int(len(M)/2 -1)
 	fbas = fs * np.cumsum(shift[1:]) / len(x)
-	fbas = fbas[:total_bins+1]
+	fbas = fbas[:total_bins]
 
 	# Assumes rasterize is full always
-	M[1:total_bins+2] = M[total_bins+1]
+	M[1:total_bins+1] = M[total_bins]
 	M[total_bins+2:] = M[total_bins:0:-1]
 
 	if normalize in ['sine','Sine','SINE','sin']:
@@ -123,13 +122,13 @@ def cqt(x, B, fs, fmin, fmax,
 
 	if len(x.shape) < 2:
 		x.shape = (len(x),1)
+
 	c,Ls = nsgtf_real(x,g,shift,phasemode,M)
 
 	# Assume rasterize is full always
-	import pdb; pdb.set_trace()
 	cDC = c[0]
 	cNyq = c[total_bins+1]
-	c = c[1:total_bins+1]
+	c = np.squeeze(np.asarray(c[1:total_bins+1]))
 
 	results = {'c':c,'g':g,'shift':shift,'M':M,'xlen':len(x),
 		'phasemode':phasemode,'rast':rasterize,'fmin':fmin,'fmax':fmax,

mcft_python/cqt_toolbox/nsgcqwin.py
@@ -87,6 +87,7 @@ def nsgcqwin(fmin,fmax,bins,sr,Ls,min_win=4,bwfac=1,fractional=0,winfun='hann',g
 
 	Lfbas = len(fbas)
 	fbas = np.concatenate(([0],fbas,[nyquist],sr-np.flip(fbas,0)))
+
 	bw = np.concatenate(([2*fmin],cqtbw,[fbas[Lfbas+2]-fbas[Lfbas]],np.flip(cqtbw,0)))
 	bw /= fftres
 	fbas /= fftres
@@ -95,7 +96,7 @@ def nsgcqwin(fmin,fmax,bins,sr,Ls,min_win=4,bwfac=1,fractional=0,winfun='hann',g
 	posit[:Lfbas+2] = np.floor(fbas[:Lfbas+2])
 	posit[Lfbas+2:] = np.ceil(fbas[Lfbas+2:])
 
-	shift = np.concatenate(([-1*posit[-1] % Lfbas], np.diff(posit)))
+	shift = np.concatenate(([-1*posit[-1] % Ls], np.diff(posit))) 
 
 	if fractional:
 		corr_shift = fbas-posit
@@ -124,11 +125,13 @@ def nsgcqwin(fmin,fmax,bins,sr,Ls,min_win=4,bwfac=1,fractional=0,winfun='hann',g
 			g.append(wfuns(winfun,window_len=bw[i]))
 
 	M = bwfac*np.ceil(M/bwfac)
-
+	
 	for i in [0,Lfbas+1]:
 		if M[i] > M[i+1]:
-			g[i] = np.ones(M[i])
-			g[i][np.floor(M[i]/2)-np.floor(M[i+1]/2):np.floor(M[i]/2)+np.ceil(M[i+1]/2)] = wfuns('hann',window_len=M[i+1])
+			g[i] = np.ones(int(M[i]))
+			start = int(np.floor(M[i]/2)-np.floor(M[i+1]/2))
+			end = int(np.floor(M[i]/2)+np.ceil(M[i+1]/2))
+			g[i][start:end] = wfuns('hann',window_len=M[i+1])
 			g[i] /= np.sqrt(M[i])
-
+	
 	return (g, shift, M)

mcft_python/cqt_toolbox/nsgtf_real.py
@@ -56,9 +56,9 @@ def nsgtf_real(f,g,shift,phasemode,M=None):
 
 	if M.size == 1:
 		M = M[0]*np.ones(N)
-		
-	f = np.fft.fft(f)
-
+	
+	f = np.fft.fft(f,axis=0)
+	
 	posit = np.cumsum(shift)-shift[0]
 
 	fill = np.sum(shift)-Ls
@@ -73,7 +73,7 @@ def nsgtf_real(f,g,shift,phasemode,M=None):
 	N = np.nonzero(N)[0][-1]
 	c = []
 	
-	for i in range(N):
+	for i in range(N+1):
 		idx = np.concatenate((np.arange(np.ceil(Lg[i]/2),Lg[i]),np.arange(np.ceil(Lg[i]/2))))
 		win_range = ((posit[i] + np.arange(-1*np.floor(Lg[i]/2),np.ceil(Lg[i]/2))) % Ls+fill) + 1
 		idx,win_range = (idx.astype(np.int32), idx.astype(np.int32))

mcft_python/cqt_toolbox/test_script.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from cqt import cqt
 import numpy as np
+import matplotlib.pyplot as plt
 
 # generate the signal    
 samp_rate = 16000 # sample rate
@@ -16,11 +17,11 @@ mod_amp.shape = (len(mod_amp),1)
 
 # harmonic signal without frequency modulation
 x1 = np.cos(2*np.pi*harmonic_freqs*time_vec)
-x1 = np.sum(x1,axis=1)
+x1 = np.sum(x1,axis=0)
 
 # harmonic signal with frequency modulation
 x2 = np.cos(2*np.pi*harmonic_freqs*time_vec+mod_amp*np.cos(2*np.pi*mod_freq*time_vec))
-x2 = np.sum(x2,axis=1)
+x2 = np.sum(x2,axis=0)
 x = x1+x2
 
 
@@ -32,4 +33,13 @@ gamma = 100
 
 
 Xcq = cqt(x, fres, samp_rate, fmin, fmax,gamma=gamma)
-Xcqt = Xcq.c
+Xcqt = Xcq['c']
+
+import pdb; pdb.set_trace()
+
+Nf,Nt = Xcqt.shape
+#fvec = Xcq['fbas']
+#tvec = np.linspace(0,time_vec[-1],Nt)
+
+plt.imshow(Xcqt.astype(np.float64), cmap='hot')
+plt.show()

mcft_python/cqt_toolbox/winfuns.py
@@ -116,10 +116,15 @@ def winfuns(window_name, sample_positions=None, window_len=None):
     if sample_positions is not None:
         pass  # TODO: should probably make a check that input is a numpy array
     elif window_len != None:
-        if window_len % 2:
-            sample_positions = np.linspace(-.5+1/(2*window_len),.5-1/(2*window_len),window_len)
+        step = 1/window_len
+        if window_len % 2 == 0:
+            first_half = np.linspace(0,.5-step,int(window_len*.5))
+            second_half = np.linspace(-.5,-step,int(window_len*.5))
+            sample_positions = np.concatenate((first_half,second_half))
         else: 
-            sample_positions = np.linspace(-.5,.5-1/window_len,window_len)
+            first_half = np.linspace(0,.5-.5*step,int(window_len*.5)+1)
+            second_half = np.linspace(-.5+.5*step,-step,int(window_len*.5))
+            sample_positions = np.concatenate((first_half,second_half))
     else:
         print("Error: invalid arguements to window function generator.")
         return None