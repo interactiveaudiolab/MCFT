6bd52f62f51e2730c3e427acac8e516acf9788fc
Fatemeh Pishdadian <fpishdadian@u.northwestern.edu>
2018-09-19 13:19:12 -0500

    no message

--- mcft_matlab/baseline_experiments/baseline_representations/inv_cft.m
+++ mcft_matlab/baseline_experiments/baseline_representations/inv_cft.m
@@ -13,27 +13,30 @@ function X=inv_cft(cft_in,params)
 % X: 2d matrix containing the reconstructed spectrogram
 
 %% Dimensions 
-[Lw1,Lw2,Nv,Nh] = size(cft_in);
+[Lw1,Lw2,Nv,Nh]=size(cft_in);
 
-Nft = params.spec_size;
-Hop = params.hop_size;
+Nft=params.spec_size;
+Hop=params.hop_size;
 
-Win = [Lw1,Lw2];
-Nvh = [Nv,Nh];
-Ovp = Win-Hop;
+Win=[Lw1,Lw2];
+Nvh=[Nv,Nh];
+Ovp=Win-Hop;
 
-Xz_dims = Nvh.*Hop+Ovp; % dimensions of the zero-padded spectrogram
+Weights = ones(Lw1,Lw2);
+%Weights = hamming(Lw1,'periodic')*hamming(Lw2,'periodic').';
+
+Xz_dims=Nvh.*Hop+Ovp; % dimensions of the zero-padded spectrogram
 
 %% Compute the inverse transform
 
-Xz = zeros(Xz_dims(1),Xz_dims(2));
-norm_w = zeros(size(Xz));
+Xz=zeros(Xz_dims(1),Xz_dims(2));
+norm_w=zeros(size(Xz));
 for i=1:Nv
-    vrange = (i-1)*Hop(1)+(1:Win(1));
+    vrange=(i-1)*Hop(1)+(1:Win(1));
     for j=1:Nh
-        hrange = (j-1)*Hop(2)+(1:Win(2));  
-        Xz(vrange,hrange)=Xz(vrange,hrange)+ifft2(cft_in(:,:,i,j)); 
-        norm_w(vrange,hrange)=norm_w(vrange,hrange)+ones(Lw1,Lw2);
+        hrange=(j-1)*Hop(2)+(1:Win(2));  
+        Xz(vrange,hrange)=Xz(vrange,hrange)+ifft2(cft_in(:,:,i,j)).* Weights;
+        norm_w(vrange,hrange)=norm_w(vrange,hrange)+ Weights.^2;
     end
 end
 
