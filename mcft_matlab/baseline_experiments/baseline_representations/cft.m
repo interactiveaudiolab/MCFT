3b357012b2314a0687ff46459213f476d065fad8
Fatemeh Pishdadian <fpishdadian@u.northwestern.edu>
2018-09-19 13:17:02 -0500

    no message

--- mcft_matlab/baseline_experiments/baseline_representations/cft.m
+++ mcft_matlab/baseline_experiments/baseline_representations/cft.m
@@ -14,27 +14,32 @@ function cft_out=cft(X,params)
 %% Dimensions and parameters
 [Nf,Nt]=size(X);
 
-Win = params.win_size;
-Hop = params.hop_size;
-Ovp = Win-Hop;
+Win=params.win_size;
+Hop=params.hop_size;
+Ovp=Win-Hop;
+
+Weights=1;
+%Weights = hamming(Win(1),'periodic')*hamming(Win(2),'periodic').';
+%Weights = blackman(Win(1))*blackman(Win(2)).';
+
 
 % compute the number of tiles 
-Nvh = ceil(([Nf,Nt]-Ovp)./Hop);
+Nvh=ceil(([Nf,Nt]-Ovp)./Hop);
 
 % zero pad the signal
-Nzft = (Nvh.*Hop)+3*Ovp; % size of the zero-padded signal
-Xz = zeros(Nzft);
-Xz(Ovp(1)+1:Ovp(1)+Nf,Ovp(2)+1:Ovp(2)+Nt) = X;
+Nzft=(Nvh.*Hop)+3*Ovp; % size of the zero-padded signal
+Xz=zeros(Nzft);
+Xz(Ovp(1)+1:Ovp(1)+Nf,Ovp(2)+1:Ovp(2)+Nt)=X;
 
 %% Compute the transform
-Nzvh = floor((Nzft-Ovp)./Hop);
+Nzvh=floor((Nzft-Ovp)./Hop);
 
-cft_out = zeros(Win(1),Win(2),Nzvh(1),Nzvh(2));
+cft_out=zeros(Win(1),Win(2),Nzvh(1),Nzvh(2));
 for i=1:Nzvh(1)
-    vrange = (i-1)*Hop(1)+(1:Win(1));
+    vrange=(i-1)*Hop(1)+(1:Win(1));
     for j=1:Nzvh(2)
-        hrange = (j-1)*Hop(2)+(1:Win(2));       
-        X_temp = fft2(Xz(vrange,hrange));
+        hrange=(j-1)*Hop(2)+(1:Win(2));       
+        X_temp=fft2(Xz(vrange,hrange).*Weights);
         cft_out(:,:,i,j) = X_temp;
     end
 end
