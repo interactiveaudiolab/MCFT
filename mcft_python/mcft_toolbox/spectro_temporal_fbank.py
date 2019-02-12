issue with filter center computation fixed

mcft_python/mcft_toolbox/spectro_temporal_fbank.py
@@ -343,6 +343,7 @@ def filt_default_centers(scale_params,rate_params):
     # compute rate filter centers
     rate_ctrs = filt_centers('rate',rate_res, rate_nfft, samprate_temp)
 
+
     return scale_ctrs, rate_ctrs
 
 
@@ -353,7 +354,7 @@ def filt_centers(filt_type,bins_per_oct,nfft,samprate):
 
     Inputs:
     filt_type: string, 'scale' or 'rate' (high pass filter is computed differently based on filter type)
-    bins_per_oct: number of scale filters per octave
+    bins_per_oct: number of scale or rate filters per octave
     nfft: number of frequencies of analysis in the scale domain
     samprate: sampling rate in the spectral domain
 
@@ -368,10 +369,11 @@ def filt_centers(filt_type,bins_per_oct,nfft,samprate):
     ctr_low = grid_res/2
 
     # center of the highest bandpass filter
-    log2_ctr_band_max = np.ceil(np.log2(samprate/2)) - 1
+    log2_ctr_band_min = np.ceil(np.log2(ctr_low))
+    log2_ctr_band_max = np.floor(np.log2(samprate/2))
 
     # centers of bandpass filters
-    ctr_band = 2 ** np.arange(0, log2_ctr_band_max+(1/bins_per_oct), 1/bins_per_oct)
+    ctr_band = 2 ** np.arange(log2_ctr_band_min, log2_ctr_band_max+(1/bins_per_oct), 1/bins_per_oct)
 
     # center of the highpass filter
     if filt_type is 'scale':
@@ -388,9 +390,9 @@ def filt_centers(filt_type,bins_per_oct,nfft,samprate):
     ctr_all = np.append(ctr_all,ctr_high)
 
     # remove repeated values(sometimes happens due to rounding)
-    scale_ctrs = np.unique(ctr_all)
+    filt_ctrs = np.unique(ctr_all)
 
-    return scale_ctrs
+    return filt_ctrs
 
 
 